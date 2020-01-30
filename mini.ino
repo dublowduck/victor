5592fa52b00b34dd482d1ce9071f2b82d74cf982
Rupert <47789156+dublowduck@users.noreply.github.com>
2020-01-30 21:56:55 +0000

    Added event variable
    Added event variable so the Arduino can hold notifications to send to the LCD.

--- mini.ino
+++ mini.ino
@@ -4,10 +4,12 @@
 #define BACKLIGHT_PIN     13
 LiquidCrystal_I2C lcd( 0x27, 2 , 1 , 0 , 4 , 5 , 6 , 7 , 3 , POSITIVE );
 
+String event = "0"; // sets the event variable to the default 0
+
 void setup()
 {
   lcd.begin(16,2); // initialize the lcd
-  lcd.setBacklight(HIGH);
+  lcd.setBacklight(HIGH); // turns the backlight on
   lcd.setCursor(1,0);
   lcd.print("Good afternoon");
   lcd.setCursor(1,1);
@@ -17,4 +19,7 @@ void setup()
 
 void loop()
 {
+  if (event !="0") {
+    lcd.print(event);
+  }
 }
