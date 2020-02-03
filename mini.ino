3b19843b418f0c74b5247e751655fb8ac6f53a7f
Rupert <47789156+dublowduck@users.noreply.github.com>
2020-02-03 21:17:52 +0000

    Update mini.ino
    Added serial communication. Added the ability to write text on the LCD by inputting it into the serial termininal. Added length detection to the lcdEvent string( <16, 16-32, >32), however it doesn't do anything useful yet. Added "lcdOn" variable, to allow the LCD to be turned off. (This defaults to "1" or on in the code). Changed the default text to "Hello there", from "Good afternoon, I`m Victor". (Not sure about that one). Renamed string "event" to "lcdEvent". Goodnight. I apologise if I missed anything.

--- mini.ino
+++ mini.ino
@@ -1,25 +1,78 @@
+// include the library code:
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 #define BACKLIGHT_PIN     13
 LiquidCrystal_I2C lcd( 0x27, 2 , 1 , 0 , 4 , 5 , 6 , 7 , 3 , POSITIVE );
 
-String event = "0"; // sets the event variable to the default 0
-
+String inData; // buffer for the incoming serial data
+String lcdEvent ="0"; // buffer for LCD text
+int lcdOn; //defines the "is the LCD on?" variable
 void setup()
 {
-  lcd.begin(16,2); // initialize the lcd
+  Serial.begin(9600);// initializes the serial comunication
+  Serial.println("Serial conection started, waiting for instructions...");
+// initialile values for the variables
+  lcdOn =1;
+  lcdEvent = inData;
+
+lcd.begin(16,2); // set up the LCD's number of columns and rows
+
+ if (lcdOn == 1) {
+  lcd.display(); // turns on the lcd
   lcd.setBacklight(HIGH); // turns the backlight on
-  lcd.setCursor(1,0);
-  lcd.print("Good afternoon");
-  lcd.setCursor(1,1);
-  lcd.print("I`m Victor");
+ }
+else if (lcdOn == 0) {
+  lcd.noDisplay(); // turns off the lcd
+  lcd.setBacklight(LOW); // turns the backlight off
+}
+
 }
 
 
 void loop()
 {
-  if (event !="0") {
-    lcd.print(event);
-  }
+   while (Serial.available() > 0)
+      {
+          char recieved = Serial.read();
+          inData += recieved;
+
+  //if (lcdEvent !="0") {
+
+    // Process message when new line character is recieved
+    if (recieved == '\n'){
+        lcdEvent = inData;
+        lcdEvent.trim();
+        Serial.print("Arduino Received: ");
+        Serial.print(inData);
+
+        if (lcdEvent.length() < 16 ) {
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print(lcdEvent);
+        }
+
+         if (lcdEvent.length() > 16) {
+          if (lcdEvent.length() < 32 ) {
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print(lcdEvent);
+            lcd.setCursor(0,1);
+            lcd.print("Cat");
+          }
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print(lcdEvent);
+          lcd.setCursor(0,1);
+          lcd.print("...");
+        }
+      }
+  //}
+  inData = ""; // Clear recieved buffer
+  delay(5000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Hello there");
+   }
 }
