64894262748b9c73c540bca95c5cb145dab81f71
Rupert <dublowduck@gmail.com>
2020-09-10 20:44:30 +0100

    setup.py now recommends a terminal
    setup.py now recommends cool-retro-term, eDEX-UI and Konsole as terminals to be used with Victor. It can also download and install cool-retro-term and Konsole, and download but not install eDEX-UI.

--- linux/setup.py
+++ linux/setup.py
@@ -66,3 +66,61 @@ clone = "git clone https://github.com/MycroftAI/mycroft-core.git"
 os.chdir(path) # Specifying the path where the cloned project needs to be copied
 os.system(clone) # Cloning
 os.system("bash /home/rupert/mycroft-core/dev_setup.sh") # Run the setup file
+
+#Customization
+print("And relax")
+time.sleep(3)
+print("That was the hardest bit")
+time.sleep(2)
+print("Now for the customization:")
+os.system("clear")
+os.system("neofetch")
+time.sleep(1)
+print("This is Victor's new home, and your computers specs")
+print("Pay close attention to the Terminal")
+print("You can run Mycroft, and by extension the victor run file, from theoretically any terminal. This will dictate the look and feel of Victor. You can use your system default terminal emulator or one of our recommendations like cool-retro-term for an old fashioned vintage feel, eDEX-UI for more of a sci-fi look or Konsole for a simple minimalist experience")
+time.sleep(4)
+print("\n")
+
+#Ask what terminal they want and then install it
+a1 = input("Which terminal emulator do you want to use?:\nR)ecomended\nS)ystem default:")
+if a1 == "s":
+    print("\n")
+    print("Ok. Victor will keep using the system default terminal emulator")
+elif a1 == "r":
+    print("\n")
+    a2 = input("Please pick a terminal to install:\nC)ool-retro-term\nE)dex-ui\nK)onsole:")
+    if a2 == "c":
+        print("\n")
+        print("Installing cool-retro-term...")
+        os.system("sudo add-apt-repository ppa:vantuz/cool-retro-term")
+        os.system("sudo apt-get update")
+        os.system("sudo apt-get install cool-retro-term")
+        time.sleep(2)
+        print("\n")
+        print("Done")
+        print("cool-retro-term installed")
+    elif a2 == "e":
+        print("\n")
+        print("Installing Edex-ui...")
+        time.sleep(2)
+        print("Ok, so you have to be a bit proactive with this one")
+        time.sleep(3)
+        print("Open this with AppImageLauncher and install it that way, I`ll wait for you")
+        time.sleep(6)
+        webbrowser.open_new_tab("https://github.com/GitSquared/edex-ui/releases/download/v2.2.2/eDEX-UI.Linux.x86_64.AppImage")
+        
+        if input("Press enter to continue...") == "hello there":
+            print("General Kenobi")
+        
+        print("\n")
+        print("Ok then")
+        print("eDEX-UI installed")
+    elif a2 == "k":
+        print("\n")
+        print("Installing Konsole...")
+        os.system("sudo apt-get install konsole")
+        time.sleep(2)
+        print("\n")
+        print("Done")
+        print("Konsole installed")
