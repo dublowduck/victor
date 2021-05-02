0d49f9e6a4b6f49e8c25f6320f7d347040642626
Rupert <47789156+dublowduck@users.noreply.github.com>
2021-05-02 22:48:13 +0100

    Added sr.py
    Added sr.py for the "Hey Victor" speech recognition and also a prompt for the user to run it from run.py. I wanted run.py to automatically start sr.py but I couldn't make it work.

--- linux/run.py
+++ linux/run.py
@@ -6,7 +6,12 @@ import webbrowser
 #Tell the user what you are doing
 print("Imported modules")
 time.sleep(0.5)
-print("Running start-up script")
+#ask the user to run sr.py
+print("----------")
+print("Run sr.py in a new terminal")
+print("----------")
+time.sleep(3)
+input("Press Enter to continue...")
 time.sleep(0.5)
 print("Starting Mycroft...")
 time.sleep(1)
