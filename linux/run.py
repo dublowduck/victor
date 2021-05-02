9931af35363ae3a81ec2b4243bf67267542e65ec
Rupert <47789156+dublowduck@users.noreply.github.com>
2021-05-02 21:15:11 +0100

    Added speech recognition script
    Added speech recognition script to detect "Hey Victor" and pass it to Mycroft. This file is run by the run.py file automictically

--- linux/run.py
+++ linux/run.py
@@ -1,13 +1,16 @@
 #Import the requirered modules
 import os
 import time
-import webbrowser
 
 #Tell the user what you are doing
 print("Imported modules")
 time.sleep(0.5)
 print("Running start-up script")
 time.sleep(0.5)
+print("Starting Speech Recognition")
+#run the speech script for "hey victor"
+os.system("python3 speechrecognition.py")
+time.sleep(1)
 print("Starting Mycroft...")
 time.sleep(1)
 
